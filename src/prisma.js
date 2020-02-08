4516e2f6c82d0ea25ff97bb0b2396273ddce5f1c
Omobolanel Aro <omobolanlearo@gmail.com>
2020-02-08 07:57:14 +0100

    removed alternative endpoint from prisma

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers/index'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: process.env.PRISMA_ENDPOINT || 'http://localhost:4466',
+    endpoint: process.env.PRISMA_ENDPOINT,
     secret: 'thisismysecretstuff',
     fragmentReplacements // required to add fragments
 });
