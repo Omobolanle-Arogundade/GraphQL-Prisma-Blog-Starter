6e053a75e3bb556fbf54c984d7345595e75c5a27
Omobolanel Aro <omobolanlearo@gmail.com>
2020-02-08 08:37:59 +0100

    changed prisma secret to use env var

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'thisismysecretstuff',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements // required to add fragments
 });
 
