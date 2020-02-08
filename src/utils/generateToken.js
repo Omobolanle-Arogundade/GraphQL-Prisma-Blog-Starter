4e7365eb6420e09f1f4d2a145573a99a3a7c2929
Omobolanel Aro <omobolanlearo@gmail.com>
2020-02-08 08:48:03 +0100

    removed jwt secret string

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 const generateToken = (id) => {
 
-    return jwt.sign({ userId: id }, "myjwtsecret", {
+    return jwt.sign({ userId: id }, process.env.JWT_SECRET , {
         expiresIn: '1hr'
     })
     
